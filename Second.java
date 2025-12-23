ab93b634cec7f650bb308bb698c2f615a689350a
Mia Johnson <tut24880@temple.edu>
2025-12-23 15:21:00 -0500

    Java

---
+++ Second.java
@@ -0,0 +1,15 @@
+class Second_DaraType{
+    public static void main(String[] args){
+        int month = 12;
+        int day =23;
+        float weight = 49.99f;
+        double height = 155.5;
+
+        String name = "美愛";
+        name = "mia";
+        System.out.println("Hello, my name is " + name);
+        System.out.println("Today is " + month + " month " + day + " day.");
+        System.out.println("My weight is " + weight + "kg."+"Heght is " + height + " cm.");
+
+    }
+}
