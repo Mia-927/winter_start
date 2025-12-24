c21c0bc6b3df568cbbd0dc0c5cdbc4061b7d3091
Mia Johnson <tut24880@temple.edu>
2025-12-24 10:35:33 -0500

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,40 @@
+/*class Third_WrapperClass{//問１
+    public static void main(String[]args){
+        System.out.println("rgea");
+        System.out.println("dsa");
+    }
+}*/
+/*public class Third {//問２
+    public static void main(String[]args){
+        int num = 10;
+        double pi = 3.14;
+        char blood = 'A';
+        boolean fun = true;
+        String msg = "Hello World";
+        System.out.println(num+""+pi+""+blood+""+fun+""+msg);
+    }
+}*/
+
+/*class Arraybug{//問３
+        public static void main(String[]args){
+        String[]name = new String[4];
+        name[0] = "suzuki";
+        name[1] = "takahashi";
+        name[2] = "tanaka";
+        name[3] = "sato";
+
+        System.out.println("names");
+        System.out.println(name[0]+"," +name[1]+"," +name[2]+"," +name[3]);
+    }
+}*/
+
+class ScoreArray{
+    public static void main(String[]args){
+        int arar[] = new int[4];
+        arar[0] = 90;
+        arar[1] = 80;
+        arar[2] = 100;
+        arar[3] = 90;
+        System.out.println(arar[0]+arar[1]+arar[2]+arar[3]);
+    }
+}
